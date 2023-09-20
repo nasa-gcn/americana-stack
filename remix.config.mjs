72c19bf23a99616f8413e3a04ce5b69ab3b29b54
Leo Singer <leo.singer@ligo.org>
2023-09-19 22:13:28 -0400

    Remove unnecessary feature flags

--- remix.config.mjs
+++ remix.config.mjs
@@ -14,12 +14,4 @@ export default {
     ? [/^(?!@?aws-sdk(\/|$))/]
     : undefined,
   postcss: true,
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 }
